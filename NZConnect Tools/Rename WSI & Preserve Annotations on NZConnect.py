afe73d97a503cfc0d3723aa7568ba12d3f6039d2
JaimeAnton <119014709+JaimeAnton@users.noreply.github.com>
2024-09-25 17:46:13 +0200

    Code outline

---
+++ NZConnect Tools/Rename WSI & Preserve Annotations on NZConnect.py
@@ -0,0 +1,18 @@
+# 
+
+# Part 1 - Creating a copy of the image on the S drive
+    #User selects the file to duplicate
+    
+    #User inputs the desired file name
+    
+    #Duplicating image on S drive
+
+
+# Part 2 - Renaming duplicated image
+    # Renaming duplicated file with file name input from user
+    
+    # Waiting until file appeis uploaded to NZconnect or running the nexr section separately
+
+
+# Part 3 - Copying annotations on NZConnect
+
